64bd29f4042e585c3b9bd6180f944713872d7e37
Ömer Yetik <omeryetik@yahoo.com>
2016-02-02 17:14:57 +0200

    Fixed a bug in Extra Credit #3

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -156,6 +156,7 @@ class ViewController: UIViewController {
             }
         } else {
             self.operate(sender)
+            userIsInTheMiddleOfTypingANumber = true
         }
     }
     //
